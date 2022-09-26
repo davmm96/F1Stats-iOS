6a80ade507c266fce2959a84224df22303a33d1e
David Melero Morant <dmelero@minsait.com>
2022-09-26 17:56:33 +0200

    Initial Commit

---
+++ F1 Stats/F1_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1_StatsApp.swift
+//  F1 Stats
+//
+//  Created by David Melero Morant on 26/9/22.
+//
+
+import SwiftUI
+
+@main
+struct F1_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
